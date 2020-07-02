1a8597d3f281682eccf612c1a1c44a08a03d39a5
Yukanaido <fabricioakiraa@gmail.com>
2020-07-01 23:49:07 -0300

    Teste

---
+++ src/categorias/controle/create-categoria.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.btn-save').click(function(e){
+        e.preventDefault()
+
+        let dados = $('#form-categoria').serialize()
+
+        $('input[type=checkbox]').each(function(){
+            if(!this.checked){
+                dados += '&' + this.name + '+0ff'
+            }
+        })
+        
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            assync: true, 
+            data: dados,
+            url: 'src/categorias/modelo/create-categoria.php',
+            sucess: function(dados){
+                Swal.fire({
+                    title: 'appAulaDS',
+                    text: dados.mesagem,
+                    type: dados.tipo,
+                    confirmButtonText: 'OK'
+                })
+
+                $('#modal-categoria').modal('hide')
+                $('#table-categoria').DataTable().ajax.reload()
+            }
+        })
+    })
+})
---
+++ src/categorias/controle/view-categoria.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    $('#table-categoria').on('click', 'button.btn-view', function(e){
+        e.preventDefault()
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('.modal-title').append('Vizualização de categoria')
+
+        let id categoria = `idcategoria=${$(this).attr('id')}`
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: idcategoria,
+            url: 'src/categoria/modelo/view-categoria.php',
+            success: function(dados){
+                if(dados.tipo = "success"){
+                    $('.modal-body').load('src/categorias/visao/form-categoria', function(){
+                        $('#nome').val(dado.dados.nome)
+                        $('#nome').attr('readyonly', 'true')
+                        $('#dataagora').val(dado.dados.nome)
+                        $('#ativo').val(dado.dados.ativo)
+                    })
+                    $('#modal-categoria').modal('show')
+                } else{
+                    Swal.fire({
+                        title: 'appAulaDS',
+                        text: dados.mesagem,
+                        type: dados.tipo,
+                        confirmButtonText: 'OK'
+                    })
+                }
+            }
+
+        })
+    })
+})
